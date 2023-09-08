5459a0e7213096c7903f0f7fe9fd0d9c55c0f3b7
Sahil Batgeri <sahilbatgeri@gmail.com>
2023-09-09 01:09:58 +0530

    Created database and landing page

--- app.js
+++ app.js
@@ -3,31 +3,19 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const path = require('path');
 const colors = require('colors');
-const routes = require('routes/routes.js');
 
 const app = express();
 const port = 3000;
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'Comment-section'
-});
-
-db.connect(err => {
-    if(err){
-        console.log('Database connection failed!'.rainbow);
-    }
-    else{
-        console.log('Database connected'.rainbow);
-    }
-});
 
 app.set('view engine' , 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'routes')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require('./routes/routes');
+
+
+app.use('/', routes);
 
 
 app.listen(port , () => {
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,70 @@
 const express = require('express');
+const mysql = require('mysql');
 
-const router = express.router();
+const router = express.Router();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'comments-section'
+});
+
+db.connect(err => {
+    if(err){
+        console.log('Database connection failed!'.rainbow);
+    }
+    else{
+        console.log('Database connected'.rainbow);
+    }
+});
 
 
 router.get('/' , (req , res) => {
-    res.render('comments');
+
+    db.query('SELECT * FROM comment', (err , results) => {
+        if(err){
+            console.log('error in fetching comment data'.rainbow);
+        }
+        else{
+            if(results.length > 0){
+
+                const data = results[0];
+                const comments = {
+                    author : data.comment_author,
+                    comment : data.comment_text
+                }
+                res.render('comments' , {comments});
+            }
+            else{
+                const comments = {
+                    author : '',
+                    comment : ''
+                }
+                res.render('comments' , {comments});
+            }
+        }
+    });
+    
+
 });
 
+router.get('/new-comment' , (req,res) => {
+    res.render('new-comment');
+});
+
+router.post('/new-comment' , (req, res) => {
+    const {author , comment} = req.body;
+
+    db.query('INSERT INTO comment (comment_id , comment_author) VALUES (?,?)', [comment , author] , (err, result) => {
+        if(err){
+            console.log('New comment not created'.rainbow);
+        }
+        else{
+            console.log('New comment added'.rainbow);
+            res.redirect('/');
+        }
+    });
+})
 
 module.exports = router;
